4da6cde7db30e417a06ecdc78e8f99435675ae7d
Srdjan Jovic <sjovic51@gmail.com>
2018-11-20 22:04:51 +0100

    Create simple interface for starting and stopping the bot

---
+++ app.py
@@ -0,0 +1,51 @@
+import wx
+import imageutil as iu
+import cv2
+import bot2048 as bot
+import pyautogui
+import numpy as np
+from threading import Thread
+
+#flag for knowing if the bot is active or not
+botActive = True
+
+#fuction to be executed when the button is pressed
+def startPlayer():
+    botActive = True
+    while botActive:
+        try:
+            screenshot = pyautogui.screenshot()
+            screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+            gameState = bot.getGameStateMatrix(screenshot)
+            bot.suggestNextMove(gameState)
+        except:
+            botActive = False
+        
+
+class BotGUI(wx.Frame):
+
+    def __init__(self, *args, **kw):
+        super(BotGUI, self).__init__(*args, **kw)
+
+        self.InitUI()
+
+    def InitUI(self):
+        panel = wx.Panel(self)
+        startButton = wx.Button(panel, label='Start', pos = (20, 20))
+        startButton.Bind(wx.EVT_BUTTON, self.start)
+
+        self.SetSize(400,200)
+        self.SetTitle('2048 Bot')
+        self.Centre()
+
+    def start(self, e):
+        Thread(target=startPlayer).start()
+
+def main():
+    app = wx.App()
+    bot = BotGUI(None)
+    bot.Show()
+    app.MainLoop()
+
+if __name__ == "__main__":
+    main()
