378585493c45668141804d8ec63f56aa81911d0e
Srdjan Jovic <sjovic51@gmail.com>
2018-12-15 14:22:17 +0100

    Add recursive fuction support for evaluating next move

--- monotonicDecreasingPlayer.py
+++ monotonicDecreasingPlayer.py
@@ -76,12 +76,41 @@ def slideLeft(gameStateMatrix, dimension):
     return flipped
 
 #determines the next move to make
-def nextMove(gameStateMatrix, recursionDepth = 3):
+def nextMove(gameStateMatrix, dimension, recursionDepth = 3):
     return 0
 
-def nextMoveRecursion():
-    return 0
+#recursivly calculates the heursitic score for a given depth and base parameter
+#base parameter determines how much the score is affected the deeper the algorithm goes
+def nextMoveRecursion(gameStateMatrix, dimension, depth, maxDepth, base = 0.9):
+    bestScore = -1
+    bestMove = 0
+    for move in range(0, 4):
+        if isMoveValid(gameStateMatrix, dimension, move):
+            newGameState = determineNextGameState(gameStateMatrix, dimension, move, generateNewTile=True)
+            score = evaluateScore(newGameState)
+            if depth != 0:
+                result_move, result_score = nextMoveRecursion(newGameState, dimension, depth - 1, maxDepth)
+                score += result_score*pow(base, maxDepth - depth + 1)
+            
+            if score > bestScore:
+                bestScore = score
+    return bestMove, bestScore
 
+#determines if the move is valid or not
+#move can be 0, 1, 2 or 3
+def isMoveValid(gameStateMatrix, dimension, move ):
+    result = 0
+    if move == 0:
+        result = slideUp(gameStateMatrix, dimension)
+    elif move == 1:
+        result = slideRight(gameStateMatrix, dimension)
+    elif move == 2:
+        result = slideDown(gameStateMatrix, dimension)
+    else: result = slideLeft(gameStateMatrix, dimension)
+
+    if (result == gameStateMatrix).sum() == dimension**2:
+        return False
+    else: return True
 
 temp = (2, 8, 2, 0, 16, 2 ,0, 0, 2, 0, 0, 0, 0, 0, 2, 0)
 temp = np.asarray(temp)
